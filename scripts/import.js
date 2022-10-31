2afbf4a1c3bc73ada01645c84d80c9879e754d5c
ZihanQ22 <zqiu3@artic.edu>
2022-10-31 18:59:39 -0500

    add stuff

--- scripts/import.js
+++ scripts/import.js
@@ -1,3 +1,26 @@
-//import stylesheets
-const STYLESHEETS
 
+// import scripts
+
+
+const SCRIPTS = [
+  `/scripts/header-footer.js`
+]
+
+SCRIPTS.forEach((script) => {
+  $.getScript(script);
+})
+
+// import stylesheets
+const STYLESHEETS = [
+  `<link rel="stylesheet" href="/stylesheets/site.css">`,
+  `<link rel="stylesheet" href="/stylesheets/fonts.css">`,
+  `<link rel="stylesheet" href="/stylesheets/header.css">`,
+  `<link rel="stylesheet" href="/stylesheets/footer.css">`,
+  `<link rel="stylesheet" href="style.css">`
+]
+
+STYLESHEETS.forEach((stylesheet) => {
+
+  $('head').append(stylesheet);
+  
+})
